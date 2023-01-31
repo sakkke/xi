35d48bf471c17794e3dbdb3ef3553786a01e0a84
sakkke <w32w64@gmail.com>
2023-01-31 20:35:06 +0900

    feat: first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# xi
+
+## xiiso
+
+### Build
+
+```sh
+xargs -a xiiso.devdeps apt install -y
+sudo ./build.sh
+```
+
+### Run
+
+```sh
+./run.sh
+```
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eu
+
+cp xi.iso user.iso
+rm -f user.qcow2
+qemu-img create -f qcow2 user.qcow2 8G
+qemu-system-x86_64 -bios OVMF.fd -enable-kvm -cpu host -hdb user.qcow2 -m 2048 user.iso
