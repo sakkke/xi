a722b0c80dd1a147b0dc79e41e0c3b2208133b10
sakkke <w32w64@gmail.com>
2023-02-20 14:48:35 +0900

    chore: add build dir

--- build.sh
+++ build.sh
@@ -6,14 +6,19 @@ if [ "$(id -u)" -ne 0 ]; then
   exec sudo "$0"
 fi
 
+setup_build() {
+  mkdir -p build
+  cd build
+}
+
 setup_rootfs() {
   rm -rf rootfs
   mkdir -p archives
   debootstrap --cache-dir="$PWD"/archives bullseye rootfs
-  xargs -a packages chroot rootfs /usr/bin/env DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -qqy install
+  xargs -a ../packages chroot rootfs /usr/bin/env DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -qqy install
   chroot rootfs /usr/bin/apt-get clean
   rm -rf rootfs/var/lib/apt/lists/*
-  cp -RT override.rootfs rootfs
+  cp -RT ../override.rootfs rootfs
   chroot rootfs /usr/sbin/useradd -mG sudo -s /bin/bash xi
   echo xi:xi | chroot rootfs /usr/sbin/chpasswd
   chroot rootfs /usr/bin/systemctl enable systemd-networkd.service
@@ -35,7 +40,7 @@ setup_efi() {
   rm -rf efi
   mkdir efi
   mount efi.img efi
-  cp -RT override.efi efi
+  cp -RT ../override.efi efi
   mkdir -p efi/EFI/BOOT efi/EFI/systemd
   cp rootfs/initrd.img rootfs/vmlinuz efi
   cp rootfs/usr/lib/systemd/boot/efi/systemd-bootx64.efi efi/EFI/BOOT/BOOTX64.EFI
@@ -56,6 +61,7 @@ setup_iso() {
   xorriso -as mkisofs -append_partition 2 0xef efi.img -o xi.iso iso
 }
 
+setup_build
 setup_rootfs
 setup_filesystem_squashfs
 setup_efi_img
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cp xi.iso user.iso
+cp build/xi.iso user.iso
 rm -f user.qcow2
 qemu-img create -f qcow2 user.qcow2 8G
 qemu-system-x86_64 -bios OVMF.fd -enable-kvm -cpu host -hdb user.qcow2 -m 2048 user.iso
