6ec967e04501bdae81599ee9a0709a811c48e18d
sakkke <w32w64@gmail.com>
2023-02-28 08:07:57 +0900

    refactor: add wraps

--- boot.sh
+++ boot.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-qemu-system-x86_64 -bios OVMF.fd -enable-kvm -drive file=user.qcow2 -cpu host -m 2048 -vga virtio
+qemu-system-x86_64 \
+  -bios OVMF.fd \
+  -enable-kvm \
+  -drive file=user.qcow2 \
+  -cpu host \
+  -m 2048 \
+  -vga virtio
--- run.sh
+++ run.sh
@@ -7,4 +7,12 @@ build_dir="${1:-build}"
 cp "$build_dir"/xi.iso user.iso
 rm -f user.qcow2
 qemu-img create -f qcow2 user.qcow2 8G
-qemu-system-x86_64 -bios OVMF.fd -enable-kvm -cpu host -drive file=user.iso,format=raw -hdb user.qcow2 -m 2048 -vga virtio
+
+qemu-system-x86_64 \
+  -bios OVMF.fd \
+  -enable-kvm \
+  -cpu host \
+  -drive file=user.iso,format=raw \
+  -hdb user.qcow2 \
+  -m 2048 \
+  -vga virtio
