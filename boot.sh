41fc4a13b9c757dc867fef6874c0bd3fd2295b88
sakkke <w32w64@gmail.com>
2023-02-28 08:06:13 +0900

    chore: make arg option

--- boot.sh
+++ boot.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-qemu-system-x86_64 -bios OVMF.fd -enable-kvm -cpu host -m 2048 -vga virtio user.qcow2
+qemu-system-x86_64 -bios OVMF.fd -enable-kvm -drive file=user.qcow2 -cpu host -m 2048 -vga virtio
